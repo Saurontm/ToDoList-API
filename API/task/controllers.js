7b0b177541edfe426948d3b0d903223d5d056435
AyahAshkanani <ayah.ash62@gmail.com>
2021-07-01 18:35:14 +0300

    creating list done

--- API/task/controllers.js
+++ API/task/controllers.js
@@ -3,3 +3,14 @@ const tasks = require("../../tasks");
 exports.tasksFetch = (req, res) => {
   res.json(tasks);
 };
+
+exports.createTask = (req,res)=> {
+const id = tasks.length +1;
+const newTask = {
+  id : id,
+  done : false,
+  ...req.body,
+};
+tasks.push(newTask);
+res.status(201).json(newTask);
+};
--- API/task/routes.js
+++ API/task/routes.js
@@ -1,11 +1,18 @@
 const express = require("express");
 
 //contoller
-const { tasksFetch } = require("./controllers");
+const { tasksFetch,createTask } = require("./controllers");
 
 const router = express.Router();
 
 //get tasks
 router.get("/", tasksFetch);
+
+//create tasks
+router.post("/",createTask);
+
+
+
+
 
 module.exports = router;
