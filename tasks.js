cc5bdb609e8bde0b175cdabdaa096608b3aa148c
Sarah Almahmid <hpsarontm@gmail.com>
2021-07-02 15:30:51 +0300

    added priority and tag to tasks

--- tasks.js
+++ tasks.js
@@ -2,12 +2,26 @@ const tasks = [
   {
     id: 1,
     title: "Finish the backend of project",
+    priority: "high",
+    tag: "work",
     done: false,
   },
   {
     id: 2,
     title: "Make trello board",
     done: true,
+  },
+  {
+    id: 3,
+    title: "tidy up work desk",
+    priority: "low",
+    tag: "home",
+    done: false,
+  },
+  {
+    id: 4,
+    title: "Design application",
+    done: false,
   },
 ];
 
